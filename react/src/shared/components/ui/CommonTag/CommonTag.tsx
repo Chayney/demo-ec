36bc136a7188560bc4ce9b96ee5f837077559a0f
Chayney <chunchunmaru33333@gmail.com>
2025-11-08 23:47:10 +0900

    共通コンポーネントの作成

---
+++ react/src/shared/components/ui/CommonTag/CommonTag.tsx
@@ -0,0 +1,14 @@
+import type { FC, ReactNode } from "react"
+import styles from './style.module.css'
+
+type CommonTagProps = {
+    children: ReactNode
+}
+
+export const CommonTag: FC<CommonTagProps> = (props) => {
+    const {children} = props;
+
+    return (
+        <span className={styles.tag}>{children}</span>
+    )
+}
